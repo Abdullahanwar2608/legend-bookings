82d75e6256cb343937c8dcbafa76790fedeb594c
Abdullah <111180606+Abdullahanwar2608@users.noreply.github.com>
2026-05-01 19:20:40 +0500

    Fix formatting and remove unnecessary comment

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
-// Importing the router you already built in router.tsx
 import { router } from './router'
 import './styles.css'
 
